cb1eccc9927c30613d4ba04d64665747f5f59dcb
Mohak-Mittal <mohakmittal012@gmail.com>
2024-09-01 07:59:08 +0530

    Complete Most of The Frontend

--- Frontend/src/components/DashboardCrew.jsx
+++ Frontend/src/components/DashboardCrew.jsx
@@ -6,7 +6,7 @@ function DashboardCrew() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const excludedKeys = ["name","_id", "__v", "routeShortName"];
+  const excludedKeys = ["name", "_id", "__v", "routeShortName"];
 
   // Get the `id` from the route parameters
   const { id } = useParams();
@@ -33,19 +33,29 @@ function DashboardCrew() {
 
   return (
     <>
-      <h1 
-      className='text-center text-4xl font-medium mt-4 text-green-500'>{data[0].name}</h1>
-    <div className="p-6 text-lg font-serif grid grid-cols-2 gap-4">
-      {filteredEntries.map(([key, value], index) => (
-        <div
-          key={index}
-          className="bg-gray-100 text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full text-left"
-        >
-          <span className="font-semibold">{key.charAt(0).toUpperCase() + key.slice(1)}:</span>
-          <span className="text-gray-500 font-thin text-sm block pt-1">{value}</span>
-        </div>
-      ))}
-    </div>
+      <h1 className='text-center text-4xl font-medium mt-4 text-green-500'>
+        {data[0].name}
+      </h1>
+      <div className="p-6 text-lg font-serif grid grid-cols-2 gap-4">
+        {filteredEntries.map(([key, value], index) => {
+          // Format value for 'ExpectedTime
+          const displayKey = key === "expectedTime" ? `${key.charAt(0).toUpperCase() + key.slice(1)} (In Minutes)` : key.charAt(0).toUpperCase() + key.slice(1);
+
+          return (
+            <div
+              key={index}
+              className="bg-gray-100 text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full text-left"
+            >
+              <span className="font-semibold">
+                {displayKey}:
+              </span>
+              <span className="text-gray-500 font-thin text-sm block pt-1">
+                {value}
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
